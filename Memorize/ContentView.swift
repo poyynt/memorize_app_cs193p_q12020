fe7f402571e59beb8aecfde89363245cf5875495
Parsa Torbati <ptorbatii@gmail.com>
2020-05-19 20:03:14 +0430

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Parsa Torbati on 5/19/20.
+//  Copyright © 2020 Parsa Torbati. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
